5b2fea53bc7656049546c6460e9a685f094d3728
Yangjianbin <851110276@qq.com>
2015-11-08 19:48:23 +0800

    ios add

---
+++ ios/testios/testios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testios
+//
+//  Created by air on 11/8/15.
+//  Copyright © 2015 weipow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
